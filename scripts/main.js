35248b5d983b3d1164d6589e9ae2f0ca52b1d1c6
Paul Vargas <paulvargasdev@gmail.com>
2018-11-04 16:57:32 -0400

    portfolio image gallery and scroll down button completed with js and jquery. Also the contact section completed. Need more social media icons if decided

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 // Scrolling Button JS (JQuery) Code
 $(function() {
     $('.scroll-down').click (function() {
-      $('html, body').animate({scrollTop: $('section.info').offset().top }, 'slow');
+      $('html, body').animate({scrollTop: $('section.ok').offset().top }, 1500);
       return false;
     });
   });
