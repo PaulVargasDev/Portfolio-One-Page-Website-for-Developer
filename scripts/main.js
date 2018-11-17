c816b9885c5050f0196e5bf2c2a1814d4caf0feb
Paul Vargas <paulvargasdev@gmail.com>
2018-11-17 12:08:50 -0400

    improved menu location + made main navbar sticky + made all link scrolling smooth

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-// Scrolling Button JQuery Code
+// Scrolling Button
 $(function() {
     $('.scroll-down').click (function() {
       $('html, body').animate({scrollTop: $('section.ok').offset().top }, 1500);
@@ -6,4 +6,53 @@ $(function() {
     });
   });
 
+window.onscroll = function() {myFunction()};
 
+// Sticky Nav Bar
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// Smooth Srolling for Links
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
